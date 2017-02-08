e91f0f3c4755aa70ffe166fdb1833a90ad8d1348
simoyassine6 <heisenberg653@gmail.com>
2017-02-08 18:28:27 +0000

    update script

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -7,7 +7,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     }
 
     console.log('Connected to MongoDB server ');
-    db.collection('user').findOneAndDelete({_id: ObjectID('589a824fec2820b524e73b69')}).then((result)=>
+    db.collection('user').findOneAndDelete({_id: ObjectID('589a6b5aec2820b524e73353')}).then((result)=>
         {
             "use strict";
             console.log(result);
---
+++ playground/mongodb-update.js
@@ -0,0 +1,38 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    // db.collection('user').findOneAndUpdate({
+    //   _id: new ObjectID('57bc4b15b3b6a3801d8c47a2')
+    // }, {
+    //   $set: {
+    //     completed: true
+    //   }
+    // }, {
+    //   returnOriginal: false
+    // }).then((result) => {
+    //   console.log(result);
+    // });
+
+    db.collection('user').findOneAndUpdate({
+        _id:  ObjectID('589b6203ec2820b524e741b0')
+    }, {
+        $set: {
+            name: 'Andrew'
+        },
+        $inc: {
+            age: 1
+        }
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    });
+
+
+   // db.close();
+});
